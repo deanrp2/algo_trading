be3d698d65f818b25e60af799c2ea13b254e9bdd
Dean Price <dean1357price1357@gmail.com>
2021-12-28 01:55:59 +0000

    updated date ranges to run

--- pull_data.py
+++ pull_data.py
@@ -51,8 +51,8 @@ def gsdata(s,fill=True,startdate=None,enddate=None,countbads=False):
         return returndata, getbad(filldata, rawdata)
 
 
-start= datetime.datetime(2021,12,16)
-end= datetime.datetime(2021,12,22)
+start= datetime.datetime(2021,12,13)
+end= datetime.datetime(2021,12,16)
 
 destination= Path("stock_data")
 
